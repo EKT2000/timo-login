64d514e2c460dbea82d2d1869c5d93a347ad8e48
ekt2000 <elias.kadiri@timo.net>
2022-07-08 09:03:03 +0200

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  TimoLoginExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-timo-login': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
